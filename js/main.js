6b496aeba9a7e229f5e477ed85a859bed5617e81
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-12-29 19:32:16 +0200

    m

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 'use strict'
 
+//FUNCTIONS FOR NAV
+
 const HOME = document.getElementById('HOME');
 HOME.addEventListener('click', function() {
-    window.location.href = 'HOME'
+    window.location.href = 'index.html'
 })
+
+const ABOUT = document.getElementById('ABOUT');
+ABOUT.addEventListener('click', () =>{
+    window.location.href = 'about.html'
+})
+
+const PROGRAM = document.getElementById('PROGRAM');
+PROGRAM.addEventListener('click', () =>{
+    PROGRAM.window.location.href = ''
+})
+const TRAINER = document.getElementById('TRAINERS')
+
+const CONTACT = document.getElementById('CONTACT')
+
+//FUNCTIONS FOR NAV END
